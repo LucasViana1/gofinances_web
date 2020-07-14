437fc2a758ee010b709c433a59c87a924cdd9d4d
Lucas Cunha <lucasviana112@gmail.com>
2020-07-14 02:04:59 -0300

    retornando ao dashboard ao realizar upload

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -29,6 +29,7 @@ const Import: React.FC = () => {
 
     try {
       await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
       console.log(err.response.error);
     }
